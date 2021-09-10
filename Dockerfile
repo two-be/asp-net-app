a67a647e1d9fd3c4cb558c72cf849ad730613347
Two <Two.Logician@icloud.com>
2021-09-11 00:59:57 +0700

    :arrow_up:

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS runtime
+FROM mcr.microsoft.com/dotnet/aspnet:5.0
 WORKDIR /app
-COPY published/ ./
+COPY  /published ./
 ENTRYPOINT ["dotnet", "AspNetApp.dll"]
--- src/AspNetApp/Startup.cs
+++ src/AspNetApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AspNetApp
 {
@@ -25,27 +26,37 @@ namespace AspNetApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetApp", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetApp v1"));
+
+            if (env.IsProduction())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
             }
 
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
         }
     }
 }
